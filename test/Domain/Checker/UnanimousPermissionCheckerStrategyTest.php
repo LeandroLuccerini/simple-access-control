97436b42cfae367540aa50dde41d60221275b1c9
Leandro Luccerini <leandro.luccerini@it.clara.net>
2023-07-08 15:43:00 +0200

    fix: Fix test removing machin depending test from github action

--- test/Domain/Checker/UnanimousPermissionCheckerStrategyTest.php
+++ test/Domain/Checker/UnanimousPermissionCheckerStrategyTest.php
@@ -2,6 +2,7 @@
 
 namespace Test\Szopen\SimpleAccessControl\Domain\Checker;
 
+use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 use Szopen\SimpleAccessControl\Domain\Action;
 use Szopen\SimpleAccessControl\Domain\Checker\AffirmativePermissionCheckerStrategy;
@@ -28,7 +29,8 @@ class UnanimousPermissionCheckerStrategyTest extends TestCase
         );
     }
 
-    public function testCheckingInTenThousandElementsPermissionCollectionMustRemainUnderTwentyMilliseconds(): void
+    #[Group('MachineDependingTest')]
+    public function testCheckingInTenThousandElementsPermissionCollectionMustRemainUnderTwoDotFiveMilliseconds(): void
     {
         $permissions = [];
         for ($i = 0; $i < 10_000; $i++) {
@@ -45,7 +47,7 @@ class UnanimousPermissionCheckerStrategyTest extends TestCase
 
         $executionTime = ($endTime - $startTime) * 1_000;
 
-        self::assertLessThan(20, $executionTime);
+        self::assertLessThan(3, $executionTime);
     }
 
     public function testActionCanBePerformedDueToExplicitAllowAndUnanimousAssertions(): void
